73367205179aaf004500920a31f77b5646fb7383
shorya tyagi <shoryatyagi29@gmail.com>
2024-03-30 10:37:33 +0530

    header removed

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,6 @@ function App() {
 
     return (
         <BrowserRouter>
-            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/:mediaType/:id" element={<Details />} />
